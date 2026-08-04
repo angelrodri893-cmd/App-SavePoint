6f8b9fea04a24cfb97ccc887d74c126789ec6e64
alexis <angelrodri893@gmail.com>
2026-08-04 18:35:42 -0500

    chore: alinear sdk y limpiar configuracion local del ide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.app_savepoint"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.example.app_savepoint"
